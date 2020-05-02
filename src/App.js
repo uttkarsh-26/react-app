9a5be189a437d09d5da8f9f9c473d8edf3b7f1f7
uttkarsh-26 <uttkarsh.26@gmail.com>
2020-05-02 16:03:41 +0530

    First React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addItem(e) {
+    var newItem;
+    e.preventDefault();
+    if (this.input.value) {
+      newItem = {
+        text: this.input.value,
+        key: Date.now(),
+      };
+    } else {
+      return;
+    }
+
+    this.setState((prevState) => {
+      return {
+        items: prevState.items.concat(newItem),
+      };
+    });
+
+    this.input.value = "";
+  }
+
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter((item) => {
+      return item.key !== key;
+    });
+    this.setState({
+      items: filteredItems,
+    });
+  }
+
+  render() {
+    var key;
+    return (
+      <div className="container w-50 align-content-center">
+        <div className="bg-info my-2">
+          <h1 className="card-header"> My ToDo List</h1>
+
+          <form onSubmit={this.addItem}>
+            <input
+              ref={(a) => (this.input = a)}
+              className="form-control"
+              placeholder="Enter a Todo task"
+            ></input>
+            <button className="btn-success my-2" type="submit">
+              Add
+            </button>
+          </form>
+        </div>
+        <TodoItems list={this.state.items} click={this.deleteItem} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function TodoItems(props) {
+  var todoEntries = props.list;
+
+  var listItems = todoEntries.map((item) => (
+    <li className="list-group-item-action my-2" key={item.key}>
+      {item.text}
+      <button
+        className="btn-danger btm-sm float-right"
+        onClick={() => props.click(item.key)}
+      >
+        Delete
+      </button>
+    </li>
+  ));
+  return <ol className="list-group">{listItems}</ol>;
+}
+
+export default ToDoList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ToDoList from './App';
 import * as serviceWorker from './serviceWorker';
+import'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToDoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
